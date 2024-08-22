3a6ebe350551fd402b6e755f4e3dba98bc4fb460
Azhar khan <azharkhanzada356@gmail.com>
2024-08-22 14:12:35 +0500

    Models

--- testing.py
+++ testing.py
@@ -6,3 +6,10 @@ from django.contrib.auth.models import User
 user = User.objects.create()
 
 print(user)
+
+from django.db import *
+class User:
+    name = "Enter the Name here: " 
+    last_name = "Enter the last name here: "
+
+    print(name, last_name)
